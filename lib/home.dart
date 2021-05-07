a5fd196ca60d2a93f3a88b9053835a029e874a85
shobith123 <shobithshobi1@gmail.com>
2021-05-07 16:49:58 +0530

    This reverts commit 237fab3b0663274a4ec16468ca23869b49683a20.

--- lib/home.dart
+++ lib/home.dart
@@ -338,6 +338,7 @@ class _HomeState extends State<Home> {
                       'category' : category,
                       'date' : date,
                           'reference' : tag
+
                     });
                 },
                 child: Padding(
